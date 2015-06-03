4d52829cece44cd08e88fe42528be95b9babd6bc
Nicolás López <nicolaslopezj@me.com>
2015-06-03 10:54:42 -0300

    change default s3 keys

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 S3.config = {
-	key: process.env.AWS_KEY || '1',
-	secret: process.env.AWS_SECRET || '1',
-	bucket: process.env.AWS_BUCKET || '1'
+	key: process.env.AWS_KEY || 'thisisnotasecret',
+	secret: process.env.AWS_SECRET || 'thisisasecret',
+	bucket: process.env.AWS_BUCKET || 'buckername'
 };
