8d8b55f8992a88753de0456370b81ded32c8e5b2
Nicolás López <nicolaslopezj@me.com>
2015-06-17 09:56:17 -0300

    links url

--- schema/users/templates.js
+++ schema/users/templates.js
@@ -1,4 +1,6 @@
 AccountsTemplates.configure({
     confirmPassword: true,
     enablePasswordChange: true,
+    termsUrl: 'http://www.venturecapital.cl/pages/Condiciones',
+    privacyUrl: 'http://www.venturecapital.cl/pages/Privacidad',
 });
