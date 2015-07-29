6b358ef858767b2b660e68b2e2998bfae16aaed3
Nicolás López <nicolaslopezj@me.com>
2015-07-29 10:58:47 -0300

    add bucket region to config

--- server/config.js
+++ server/config.js
@@ -1,7 +1,8 @@
 S3.config = {
 	key: process.env.AWS_KEY || 'thisisnotasecret',
 	secret: process.env.AWS_SECRET || 'thisisasecret',
-	bucket: process.env.AWS_BUCKET || 'buckername'
+	bucket: process.env.AWS_BUCKET || 'buckername',
+	region: process.env.AWS_REGION || 'us-east-1'
 };
 
 if (Meteor.settings.google) {
